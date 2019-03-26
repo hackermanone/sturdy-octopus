34149d4ef9f6f50a644b0fd7d6505f112fadf41c
Ricky Chen <ricky.chen98@hotmail.com>
2019-03-26 10:38:57 -0400

    Styling

--- sturdy-octopus-2/src/js/index.js
+++ sturdy-octopus-2/src/js/index.js
@@ -13,12 +13,37 @@ let portfoliosPath = path.join(desktopPath, process.env.PROJECT_NAME, 'portfolio
 fs.readdir(portfoliosPath, (err, files) => {
     if (err) throw err;
     files.forEach(file => {
-        let root = document.getElementById('root');
-        let textNode = document.createTextNode(file);
-        let br = document.createElement("br");
+        let root = document.getElementById('root'),
+            portfolioDiv = document.createElement('div'),
+            text = document.createTextNode(file),
+            br = document.createElement('br'),
+            optionsDiv = document.createElement('div'),
+            optionsText = document.createTextNode('Delete');
 
-        root.appendChild(textNode);
+        optionsDiv.appendChild(optionsText);
+        optionsDiv.classList.add('portfolio-block-options');
+        portfolioDiv.appendChild(text);
+        portfolioDiv.classList.add('portfolio-block');
+        
+
+        portfolioDiv.addEventListener('click', handlePortfolioClick);
+        optionsDiv.addEventListener('click', handleDeleteClick);
+
+        root.appendChild(portfolioDiv);
+        root.appendChild(optionsDiv);
         root.appendChild(br);
     })
 })
 
+/**
+ * event listener
+ */
+function handlePortfolioClick() {
+    // open up new window, with portfolio details
+    console.log('portfolio')
+}
+
+function handleDeleteClick() {
+    // delete portfolio entry in folder and refresh window
+    console.log('delete')
+}

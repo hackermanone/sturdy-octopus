66eb6d28feffef8e9df7b80a020c6ccdf2235d91
Ricky Chen <ricky.chen98@hotmail.com>
2019-04-24 11:31:17 -0400

    refactor

--- sturdy-octopus-6/src/index.js
+++ sturdy-octopus-6/src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import chat from './reducers';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { addUser } from './actions'
+import './index.css'
+import App from './App'
+import chat from './reducers'
 
-const store = createStore(chat);
+const store = createStore(chat)
+
+// register ourselves as present in the chat
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
